1422ca581895625106ba1bf35c15d8af4b30ec2d
Teerth Patel <teerth1205@gmail.com>
2020-03-05 02:17:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end 
+  
+  post '/piglatinize' do 
+    erb :pig
+  end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+class  PigLatinizer
+
+  
+  
+def piglatinize(text)
+  @text = text
+  arr2 = []
+  arr1  = @text.split(" ")
+  arr1.map do |w|
+  arr2 << pig_latin(w)
+  end 
+  new = arr2.join(" ")
+  binding.pry
+end 
+    
+    def pig_latin(word)
+      
+    letters = word.split("")
+    if vowel?(letters[0]) 
+       word << "way"
+       return word
+    elsif letters.size > 1
+      until vowel?(letters[0]) 
+        letters << letters.shift
+      end
+      letters.join << "ay"
+    end 
+  
+  end
+
+
+def vowel?(letter)
+    letter.downcase
+    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+end
+      
+end 
